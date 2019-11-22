8bcaeab0c29f628e8780c9ad0c169c8f0067f2cf
Nikhil5320 <53870479+Nikhil5320@users.noreply.github.com>
2019-11-22 16:42:14 +0530

    Create Threadslab9.java

---
+++ Threadslab9.java
@@ -0,0 +1,44 @@
+/*9.Write a program which creates two threads, one thread displaying “BMS College of Engineering” once every ten seconds and another displaying “CSE” 
+once every two seconds.*/
+
+import java.util.*;
+
+class BMS extends Thread
+{
+ public void run()
+ {
+  for(int i=0;i<5;i++)
+  {
+   System.out.println("BMS College of Engineering");
+   try{
+       Thread.sleep(10000);
+      }catch(Exception e){};
+  }
+ }
+}
+
+class CSE extends Thread
+{
+ public void run()
+ {
+  for(int i=0;i<5;i++)
+  {
+   System.out.println("Computer Science and Engineering");
+   try{
+       Thread.sleep(10000);
+      }catch(Exception e){};
+  }
+ }
+}
+
+public class Threadlab9
+{
+ public static void main(String args[])
+ {
+  BMS a1=new BMS();
+  CSE a2=new CSE();
+
+  a1.start();
+  a2.start();
+ }
+}
